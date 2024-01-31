39a981cbd4ac93536eee43f7e4a85b03742b7996
Erik C. Thauvin <erik@thauvin.net>
2024-01-30 23:29:41 -0800

    Bumped bld to 1.8.0

--- src/main/java/rife/bld/extension/GeneratedVersion.java
+++ src/main/java/rife/bld/extension/GeneratedVersion.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 the original author or authors.
+ * Copyright 2023-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,6 +39,8 @@ public class GeneratedVersion {
 
     /**
      * Returns the class file.
+     *
+     * @return the class file
      */
     public File getClassFile() {
         return classFile;
