d6a1919b5eb878409b130926f21aa6ea6a435bee
Erik C. Thauvin <erik@thauvin.net>
2024-05-09 20:54:52 -0700

    Bumped bld to version 1.9.1

--- src/bld/java/rife/bld/extension/GeneratedVersionOperationBuild.java
+++ src/bld/java/rife/bld/extension/GeneratedVersionOperationBuild.java
@@ -44,7 +44,7 @@ public class GeneratedVersionOperationBuild extends Project {
         repositories = List.of(MAVEN_CENTRAL, RIFE2_RELEASES);
 
         scope(compile)
-                .include(dependency("com.uwyn.rife2", "bld", version(1, 9, 0)));
+                .include(dependency("com.uwyn.rife2", "bld", version(1, 9, 1)));
         scope(test)
                 .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 10, 2)))
                 .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 10, 2)))
