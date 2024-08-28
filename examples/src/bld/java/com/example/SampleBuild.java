46b9a52215303bea9c6cf3b1a574d83aa1feefc0
Erik C. Thauvin <erik@thauvin.net>
2024-08-28 00:55:11 -0700

    Bumped JUnit to version 5.11.0

--- examples/src/bld/java/com/example/SampleBuild.java
+++ examples/src/bld/java/com/example/SampleBuild.java
@@ -27,8 +27,8 @@ public class SampleBuild extends Project {
 
         repositories = List.of(MAVEN_CENTRAL, RIFE2_RELEASES);
         scope(test)
-                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 10, 3)))
-                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 10, 3)));
+                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 11, 0)))
+                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 11, 0)));
     }
 
     public static void main(String[] args) {
