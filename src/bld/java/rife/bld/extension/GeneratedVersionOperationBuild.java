77dcb9f32114fdb0eeb7915e009f96e9db54c3e7
Erik C. Thauvin <erik@thauvin.net>
2025-08-16 00:10:48 -0700

    Use $NPM_PACKAGES to locate xunit-view

--- src/bld/java/rife/bld/extension/GeneratedVersionOperationBuild.java
+++ src/bld/java/rife/bld/extension/GeneratedVersionOperationBuild.java
@@ -118,16 +118,19 @@ public class GeneratedVersionOperationBuild extends Project {
             ex = e;
         }
 
-        var xunitViewer = new File("/usr/bin/xunit-viewer");
-        if (xunitViewer.exists() && xunitViewer.canExecute()) {
-            var reportsDir = "build/reports/tests/test/";
+        var npmPackagesEnv = System.getenv("NPM_PACKAGES");
+        if (npmPackagesEnv != null && !npmPackagesEnv.isEmpty()) {
+            var xunitViewer = Path.of(npmPackagesEnv, "bin", "xunit-viewer").toFile();
+            if (xunitViewer.exists() && xunitViewer.canExecute()) {
+                var reportsDir = "build/reports/tests/test/";
 
-            Files.createDirectories(Path.of(reportsDir));
+                Files.createDirectories(Path.of(reportsDir));
 
-            new ExecOperation()
-                    .fromProject(this)
-                    .command(xunitViewer.getPath(), "-r", testResultsDir, "-o", reportsDir + "index.html")
-                    .execute();
+                new ExecOperation()
+                        .fromProject(this)
+                        .command(xunitViewer.getPath(), "-r", testResultsDir, "-o", reportsDir + "index.html")
+                        .execute();
+            }
         }
 
         if (ex != null) {
