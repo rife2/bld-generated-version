779380053814ed77ad95d48a8b57a1c944a09d4f
Erik C. Thauvin <erik@thauvin.net>
2026-05-24 00:19:27 -0700

    Bump JUnit to version 6.1.0

--- examples/src/bld/java/com/example/SampleBuild.java
+++ examples/src/bld/java/com/example/SampleBuild.java
@@ -26,7 +26,7 @@ public class SampleBuild extends Project {
 
         repositories = List.of(MAVEN_CENTRAL, RIFE2_RELEASES, RIFE2_SNAPSHOTS);
 
-        var junit = version(6, 0, 3);
+        var junit = version(6, 1, 0);
         scope(test)
                 .include(dependency("org.junit.jupiter", "junit-jupiter", junit))
                 .include(dependency("org.junit.platform", "junit-platform-console-standalone", junit));
