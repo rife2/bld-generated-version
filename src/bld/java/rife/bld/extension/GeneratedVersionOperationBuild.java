69196ff8adb1633071c5d99af693784fee02af63
Erik C. Thauvin <erik@thauvin.net>
2026-09-09 20:16:30 -0700

    Bump Extensions Tools to version 1.3.0

--- src/bld/java/rife/bld/extension/GeneratedVersionOperationBuild.java
+++ src/bld/java/rife/bld/extension/GeneratedVersionOperationBuild.java
@@ -47,7 +47,7 @@ public class GeneratedVersionOperationBuild extends Project {
         var junit = version(6, 1, 3);
         scope(compile)
                 .include(dependency("com.uwyn.rife2", "bld-extensions-tools",
-                        version(1, 3, 0, "SNAPSHOT")))
+                        version(1, 3, 0)))
                 .include(dependency("com.uwyn.rife2", "bld",
                         version(2, 3, 1, "SNAPSHOT")));
         scope(provided)
